0a94633160b00bc5bfd2a8513db688484aa68385
zuoqiang <756465185@qq.com>
2017-05-28 22:36:50 +0800

    创建一个新的 entity

--- src/main/java/com/zuoqiang/entity/Article.java
+++ src/main/java/com/zuoqiang/entity/Article.java
@@ -8,11 +8,46 @@ public class Article {
     private User user;
     private String title;
     private String content;
-    public Article(){
 
-    }
-    public Article(int id,User user,String title,String content){
-       this.id=id;
+    public Article() {
     }
 
+    public Article(int id, User user, String title, String content) {
+        this.id = id;
+        this.user = user;
+        this.title = title;
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
