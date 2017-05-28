28a8501de1d4762b47a63e9d9cba672e59da6527
zuoqiang <756465185@qq.com>
2017-05-28 21:51:35 +0800

    remove some things

---
+++ src/main/java/com/zuoqiang/entity/Article.java
@@ -0,0 +1,15 @@
+package com.zuoqiang.entity;
+
+/**
+ * Created by ZUO on 2017/5/28.
+ */
+public class Article {
+    private int id;
+    private User user;
+    private String title;
+    private String content;
+    public Article(){
+
+    }
+
+}
